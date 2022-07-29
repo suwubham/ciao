4cf1fb2d958d5f25a68373ee90bc2eb666694298
Saral Sainju <sainju.saral433@gmail.com>
2022-07-29 09:57:02 +0545

    register page designed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import RegisterLogin from "./myComponents/Register-Login"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Homepage from './myComponents/Homepage'
+import Blogs from './myComponents/Blogs'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <RegisterLogin />
+      <Homepage /> */}
+      <div className="route-div">
+      <Router>
+        <div className="Routes">
+          <ul>
+            <li>
+              <Link to="/">Register Login</Link>
+            </li>
+            <li>
+              <Link to="/Homepage">Homepage</Link>
+            </li>
+            <li>
+              <Link to="/Blogs">Blogs</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route exact path='/' element={< RegisterLogin />}></Route>
+            <Route exact path='/homepage' element={< Homepage />}></Route>
+            <Route exact path='/blogs' element={< Blogs />}></Route>
+          </Routes>
+        </div>
+      </Router>
+      </div>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/myComponents/Register-Login.js
@@ -0,0 +1,155 @@
+import React, { useState } from 'react'
+import Logo from 'C:/Users/hp/Desktop/react/reactapp/src/Logo.jpg';
+
+export default function RegisterLogin() {
+
+  let [RegisterMode, setRegisterMode] = useState("signin")
+
+  const changeRegisterMode = () => {
+    setRegisterMode(RegisterMode == "signin" ? "signup" : "signin")
+  }
+
+  if (RegisterMode == "signin") {
+    return (
+      <div className="Main-div">
+        <div className="Register-form-container">
+          <form className="Register-form">
+            <div className="Register-form-content">
+              <h3 className="Register-form-title">Sign In</h3>
+              <div className="text-center">
+                Not registered yet?{" "}
+                <span className="link-primary" onClick={changeRegisterMode}>
+                  Sign Up
+                </span>
+              </div>
+              <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="Enter email"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-secondary">
+                  Submit
+                </button>
+              </div>
+              <p className="text-center mt-2">
+                Forgot <a href="#" className="link-pwem">password?</a>
+              </p>
+            </div>
+          </form>
+        </div>
+        <div className="imageArea">
+          <h1>
+            Generate your own Art
+          </h1>
+          <h3>
+            Work with complex math graphs and <br />other image templates to create your art.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+            repudiandae eligendi impedit beatae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+            repudiandae eligendi impedit beatae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+            repudiandae eligendi impedit beatae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+            repudiandae eligendi impedit beatae!
+          </h3>
+          <img src={Logo} height={100} width={100} />
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div className="Main-div">
+      <div className="Register-form-container">
+        <form className="Register-form">
+          <div className="Register-form-content">
+            <h3 className="Register-form-title">Sign Up</h3>
+            <div className="text-center">
+              Already registered?{" "}
+              <span className="link-primary" onClick={changeRegisterMode}>
+                Sign In
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>First Name</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="e.g Max"
+              />
+              <label>Last Name</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="e.g Aarons"
+              />
+
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="maxaarons@gmail.comm"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Password"
+              />
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-secondary">
+                  Submit
+                </button>
+              </div>
+              <p className="text-center mt-2">
+                Forgot <a href="#">password?</a>
+              </p>
+            </div>
+          </div>s
+        </form>
+      </div>
+      <div className="imageArea">
+        <h1>
+          Generate your own Art
+        </h1>
+        <h3>
+          Work with complex math graphs and <br />other image templates to create your art.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+          repudiandae eligendi impedit beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+          repudiandae eligendi impedit beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+          repudiandae eligendi impedit beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+          repudiandae eligendi impedit beatae!
+        </h3>
+        <img src={Logo} height={100} width={100} />
+      </div>
+    </div>
+  )
+
+}
+
