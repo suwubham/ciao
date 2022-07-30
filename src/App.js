c4d7200c3b9886fdbe26e15b2317b05fe21ae235
Shubham Shakya <shubhamshakya369@gmail.com>
2022-07-30 11:53:00 +0545

    added assets folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css'
-import RegisterLogin from "./Components/Register-Login"
-import "bootstrap/dist/css/bootstrap.min.css"
-import Homepage from './Components/Homepage'
-import Blogs from './Components/Blogs'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import AppRouter from './Components/AppRouter'
+import "./App.css";
+import RegisterLogin from "./components/Register-Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./components/Homepage";
+import Blogs from "./components/Blogs";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
@@ -18,29 +18,70 @@ function App() {
         
       </div> */}
 
-      <navbar class="navbar sticky-top navbar-expand-lg bg-secondary" style={{postion:"fixed"}} >
+      <navbar
+        class="navbar sticky-top navbar-expand-lg bg-secondary"
+        style={{ postion: "fixed" }}
+      >
         <div class="container-fluid">
-          <a class="navbar-title" href="http://localhost:3000/home">CIAO</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a class="navbar-title" href="http://localhost:3000/home">
+            CIAO
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="http://localhost:3000/signin">Register</a>
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="http://localhost:3000/signin"
+                >
+                  Register
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="http://localhost:3000/blogs">Blogs </a>
+                <a class="nav-link" href="http://localhost:3000/blogs">
+                  Blogs{" "}
+                </a>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Draw
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">From Template</a></li>
-                  <li><a class="dropdown-item" href="#">From Image</a></li>
-                  <li><hr class="dropdown-divider"/></li>
-                  <li><a class="dropdown-item" href="#">Blah Blah Blah</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      From Template
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      From Image
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Blah Blah Blah
+                    </a>
+                  </li>
                 </ul>
               </li>
               <li class="nav-item">
@@ -48,8 +89,15 @@ function App() {
               </li>
             </ul>
             <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
             </form>
           </div>
         </div>
@@ -60,4 +108,3 @@ function App() {
 }
 
 export default App;
-
--- src/Components/Register-Login.js
+++ src/Components/Register-Login.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react'
-import Logo from '../Logo.jpg';
-import './Register-Login.css'
-
+import React, { useState } from "react";
+import Logo from "../assets/logo.png";
+import "./Register-Login.css";
 
 export default function RegisterLogin() {
-
-  let [RegisterMode, setRegisterMode] = useState("signin")
+  let [RegisterMode, setRegisterMode] = useState("signin");
 
   const changeRegisterMode = () => {
-    setRegisterMode(RegisterMode == "signin" ? "signup" : "signin")
-  }
+    setRegisterMode(RegisterMode == "signin" ? "signup" : "signin");
+  };
 
   if (RegisterMode === "signin") {
     return (
@@ -41,39 +39,43 @@ export default function RegisterLogin() {
                 />
               </div>
               <div className="d-grid gap-2 mt-3">
-                <button type="submit" className="submit-btn" >
+                <button type="submit" className="submit-btn">
                   Log In
                 </button>
               </div>
               <p className="text-center mt-2">
-                Forgot <a href="#" className="link-pwem">password?</a>
+                Forgot{" "}
+                <a href="#" className="link-pwem">
+                  password?
+                </a>
               </p>
             </div>
           </form>
         </div>
         <div className="imageArea">
-          <h1>
-            Generate your own Art
-          </h1>
+          <h1>Generate your own Art</h1>
           <h3>
-            Work with complex math graphs and <br />other image templates to create your art.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
-            repudiandae eligendi impedit beatae!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
-            repudiandae eligendi impedit beatae!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
-            repudiandae eligendi impedit beatae!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-            excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+            Work with complex math graphs and <br />
+            other image templates to create your art. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem doloremque quis a atque quidem
+            esse excepturi, architecto amet pariatur impedit. Fuga perspiciatis
+            pariatur temporibus veniam. Quod repudiandae eligendi impedit
+            beatae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            doloremque quis a atque quidem esse excepturi, architecto amet
+            pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+            repudiandae eligendi impedit beatae! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Rem doloremque quis a atque quidem
+            esse excepturi, architecto amet pariatur impedit. Fuga perspiciatis
+            pariatur temporibus veniam. Quod repudiandae eligendi impedit
+            beatae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            doloremque quis a atque quidem esse excepturi, architecto amet
+            pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
             repudiandae eligendi impedit beatae!
           </h3>
           <img src={Logo} height={100} width={100} />
         </div>
       </div>
-    )
+    );
   }
   return (
     <div className="Main-div">
@@ -100,7 +102,6 @@ export default function RegisterLogin() {
                 className="form-control mt-1"
                 placeholder="e.g Aarons"
               />
-
             </div>
             <div className="form-group mt-3">
               <label>Email address</label>
@@ -123,35 +124,37 @@ export default function RegisterLogin() {
                 </button>
               </div>
               <p className="text-center mt-2">
-                Forgot <a href="#" className="link-pwem">password?</a>
+                Forgot{" "}
+                <a href="#" className="link-pwem">
+                  password?
+                </a>
               </p>
             </div>
           </div>
         </form>
       </div>
       <div className="imageArea">
-        <h1>
-          Generate your own Art
-        </h1>
+        <h1>Generate your own Art</h1>
         <h3>
-          Work with complex math graphs and <br />other image templates to create your art.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
-          repudiandae eligendi impedit beatae!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
-          repudiandae eligendi impedit beatae!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
-          repudiandae eligendi impedit beatae!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
-          excepturi, architecto amet pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+          Work with complex math graphs and <br />
+          other image templates to create your art. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+          excepturi, architecto amet pariatur impedit. Fuga perspiciatis
+          pariatur temporibus veniam. Quod repudiandae eligendi impedit beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+          doloremque quis a atque quidem esse excepturi, architecto amet
+          pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
+          repudiandae eligendi impedit beatae! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Rem doloremque quis a atque quidem esse
+          excepturi, architecto amet pariatur impedit. Fuga perspiciatis
+          pariatur temporibus veniam. Quod repudiandae eligendi impedit beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+          doloremque quis a atque quidem esse excepturi, architecto amet
+          pariatur impedit. Fuga perspiciatis pariatur temporibus veniam. Quod
           repudiandae eligendi impedit beatae!
         </h3>
         <img src={Logo} height={100} width={100} />
       </div>
     </div>
-  )
-
+  );
 }
-
