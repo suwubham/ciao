2d9356ae35bd1777045da5acce7464c74c56a64c
Shubham Shakya <shubhamshakya369@gmail.com>
2022-08-09 23:23:33 +0545

    renamed components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
