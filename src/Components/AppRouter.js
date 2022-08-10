a8cdb31bdb46b7214b97dce1850e2d5b99c5c808
Shubham Shakya <shubhamshakya369@gmail.com>
2022-08-10 07:06:04 +0545

    bug fixed

--- src/Components/AppRouter.js
+++ src/Components/AppRouter.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom"
-import Navbar from './Navbar'
-import LandingPage from './LandingPage'
-import RegisterLogin from './Register-Login'
-import Homepage from './Homepage'
-import YourArts from './YourArts'
-import Profile from './Profile'
-import Template from './Template'
-import About from './About'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import LandingPage from "./LandingPage";
+import RegisterLogin from "./Register-Login";
+import Homepage from "./Homepage";
+import YourArts from "./YourArts";
+import Profile from "./Profile";
+import Template from "./Template";
+import About from "./About";
+import Phyllotaxis from "./art/phyllotaxis";
 export default function AppRouter() {
   return (
     <Routes>
