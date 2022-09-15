9d277c169aa626881f66a57bd93932b219f51e18
Shubham Shakya <shubhamshakya369@gmail.com>
2022-09-15 19:14:53 +0545

    designed home page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,53 +1,70 @@
-import React from 'react'
-import '../styles/LandingPage.css'
-import Logo from '../assets/ciao-3.png'
-import Link1 from '../assets/facebook-512.png'
-import Link2 from '../assets/instagram-logo-2433.png'
-import Link3 from '../assets/twlogo2.png'
+import React from "react";
+import "../styles/LandingPage.css";
+import Logo from "../assets/ciao-3.png";
+import Link1 from "../assets/facebook-512.png";
+import Link2 from "../assets/instagram-logo-2433.png";
+import Link3 from "../assets/twlogo2.png";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import RegisterLogin from './Register-Login'
-
+import RegisterLogin from "./Register-Login";
+import Homepage from "./Homepage";
 export default function LandingPage() {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  return (
+    <>
+      <Routes>
+        <Route path="/signin" element={<RegisterLogin />} />
+        <Route path="/home" element={<Homepage />} />
+      </Routes>
 
-    return (
-        <>
-            <Routes>
-                <Route path="/signin" element={<RegisterLogin />} />
-            </Routes>
-
-            <div className="landingpage-div">
-                <header>
-                    <h3 className="title">C I A O</h3>
-                    {/* <img src="#" className="ciaoimg" /> */}
-                </header>
-                <div className="text">
-                    <div className="maintext">
-                        <h1 className="TextArea">
-                            Generate your own art
-                        </h1>
-                        <h3>
-                            Work with complex math graphs and <br />other image templates to create your art.
-                        </h3>
-                        <button type="submit" className="signin" onClick={()=>navigate('/signin')}>Get Started</button>
-                    </div>
-                    <div className="image">
-                        <img src={Logo} className="ciaoimg" />
-                    </div>
-                </div>
-                <footer className="logos">
-                    <div className="links-list">
-                        <ul>
-                            <li><a href="#"><img src={Link1} id="fb" /></a></li>
-                            <li><a href="#"><img src={Link2} id="insta" /></a></li>
-                            <li><a href="#"><img src={Link3} id="tw" /></a></li>
-                        </ul>
-                    </div>
-                    <p className="copyright">CIAO 2022 copyright - All rights reserved</p>
-                </footer>
-            </div>
-            {/* <footer>
+      <div className="landingpage-div">
+        <header>
+          <h3 className="title">C I A O</h3>
+          {/* <img src="#" className="ciaoimg" /> */}
+        </header>
+        <div className="text">
+          <div className="maintext">
+            <h1 className="TextArea">Generate your own art</h1>
+            <h3>
+              Work with complex math graphs and <br />
+              other image templates to create your art.
+            </h3>
+            <button
+              type="submit"
+              className="signin"
+              onClick={() => navigate("/home")}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="image">
+            <img src={Logo} className="ciaoimg" />
+          </div>
+        </div>
+        <footer className="logos">
+          <div className="links-list">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src={Link1} id="fb" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={Link2} id="insta" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={Link3} id="tw" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <p className="copyright">CIAO 2022 copyright - All rights reserved</p>
+        </footer>
+      </div>
+      {/* <footer>
                 <div className="row">
                     <div className="col">
                         <p>A web tool that enables the users to produce generative art of different kinds</p>
@@ -71,7 +88,6 @@ export default function LandingPage() {
                 <hr/>
 
             </footer> */}
-
-        </>
-    )
+    </>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,138 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import '../styles/Navbar.css'
-import Logo from '../assets/ciaologo3.png';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Navbar.css";
+import Logo from "../assets/ciao-3.png";
 
 export default function Navbar() {
-
   let navigate = useNavigate();
 
   return (
-    <nav className="navbar navbar-custom sticky-top navbar-expand-lg " style={{ postion: "fixed" }} >
+    <nav
+      className="navbar navbar-custom sticky-top navbar-expand-lg "
+      style={{ postion: "fixed" }}
+    >
       <div className="container-fluid">
-        <a className="navbar-title navbar-custom" onClick={()=>navigate('/home')}><img src={Logo} height={30} width={75} /></a>
+        <a
+          className="navbar-title navbar-custom"
+          onClick={() => navigate("/home")}
+        >
+          {/* <img src={Logo} height={30} width={75} /> */}
+          CIAO
+        </a>
 
-        <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler custom-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon my-toggler"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link navbar-custom" aria-current="page" onClick={()=>navigate('/about')}>About</a>
+              <a
+                className="nav-link navbar-custom navvv"
+                aria-current="page"
+                onClick={() => navigate("/home")}
+              >
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link navbar-custom" onClick={()=>navigate('/yourarts')}>Your Arts </a>
+              <a
+                className="nav-link navbar-custom navvv"
+                aria-current="page"
+                onClick={() => navigate("/about")}
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link navbar-custom"
+                onClick={() => navigate("/yourarts")}
+              >
+                Your Arts{" "}
+              </a>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle  navbar-custom" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a
+                className="nav-link dropdown-toggle  navbar-custom"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Draw
               </a>
               <ul className="dropdown-menu navbar-custom">
-                <li><a className="dropdown-item navbar-custom" onClick={()=>navigate('/template')}>From Template</a></li>
-                <li><a className="dropdown-item navbar-custom" href="#">From Image</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item navbar-custom" href="#">Blah Blah Blah</a></li>
+                <li>
+                  <a
+                    className="dropdown-item navbar-custom"
+                    onClick={() => navigate("/template")}
+                  >
+                    From Template
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item navbar-custom" href="#">
+                    From Image
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item navbar-custom" href="#">
+                    ASCII art
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>
+
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="search-btn" type="submit">Search</button>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link navbar-custom navvv special"
+                  aria-current="page"
+                  onClick={() => navigate("/signin")}
+                >
+                  Login
+                  <span class="material-symbols-outlined">login</span>
+                </a>
+              </li>
+            </ul>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="search-btn" type="submit">
+              Search
+            </button>
           </form>
-          {/* <img src={Logo} height={30} width={100}/> */}
-          <ul className="navbar-nav">
+
+          {/* <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link navbar-custom" id="Profile-btn" onClick={()=>navigate('/profile')}>Profile</a>
+              <a
+                className="nav-link navbar-custom"
+                id="Profile-btn"
+                onClick={() => navigate("/profile")}
+              >
+                Profile
+              </a>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </nav>
-  )
+  );
 }
