213570ac003fcefb70ca05a861b09078f6b49482
Prasiddhidahal <87261517+Prasiddhidahal@users.noreply.github.com>
2022-10-20 23:24:05 +0545

    Designed About us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,90 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../styles/About.css";
-import LoggedNavbar from "../components/Navbar_logged";
-import authService from "../services/auth.service";
+//import LoggedNavbar from "../components/Navbar_logged";
+//import authService from "../services/auth.service";
+//import ciao from "../assets/ciaologo.png";
+import girl1 from"../assets/girl1.jpg";
+import boy1 from"../assets/boy1.jpg"
+import boy2 from"../assets/boy2.jpg"
+import logo from"../assets/ciaologo3.png"
+//import authService from "../services/auth.service";
 
 export default function About() {
   return (
-    <>
-      {authService.getCurrentUser() ? <LoggedNavbar /> : <Navbar />}
-      <div className="about">
-        <Navbar />
-        <div>About</div>
-        <button
-          class="btn btn-primary"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapseExample"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Button with data-target
-        </button>
-        <div class="collapse" id="collapseExample">
-          <div class="card card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-            labore wes anderson cred nesciunt sapiente ea proident.
-          </div>
+  <>
+   <Navbar />
+   <div id="wrapper">
         </div>
+   <div className="spacer layer1 flip"></div>
+        
+        <div className="ani">
+        </div>
+   <div class="about-section">
+  <h1>About Us Page</h1>
+  
+  <p>We create generative art bla bla</p>
+  <p>bla bla</p>
+  
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+    <img src={girl1}alt="girl1"/>
+      <div class="container">
+        <h2>Prasiddhi Dahal</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>prasiddhidahal1@gmail.com</p>
+        <p><button class="button">Contact</button></p>
       </div>
-    </>
-  );
-}
+    </div>
+    </div>
+    
+  
+
+  <div class="column">
+    <div class="card">
+    <img src={boy1}alt="boy1" />
+      <div class="container">
+        <h2>Shubham Shakya</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>shakyashubham@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+    <img src={boy2}alt="boy2"/>
+      <div class="container">
+        <h2>Saral Sainju</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>saral@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="column">
+    <div class="card">
+    <img src={logo}alt="logo"/>
+      <div class="container">
+        <h2>Kriti Gautam</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>gautamkriti@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</>
+  )
+  }
+
+      
+        
