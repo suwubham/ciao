9a4deb29d1ec00344d619eb290065610dc5a8939
Shubham Shakya <93587505+suwubham@users.noreply.github.com>
2022-11-01 20:40:27 +0545

    Prasiddhi frontend (#57)
    * Designed About us page
    
    Co-authored-by: Prasiddhidahal <87261517+Prasiddhidahal@users.noreply.github.com>

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,53 +1,92 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../styles/About.css";
-import LoggedNavbar from "../components/Navbar_logged";
-import authService from "../services/auth.service";
-import Box from "@mui/material/Box";
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
-import SaveIcon from "@mui/icons-material/Save";
-import PrintIcon from "@mui/icons-material/Print";
-import ShareIcon from "@mui/icons-material/Share";
-
-function help() {
-  console.log("hello world");
-}
-
-const actions = [
-  { icon: <FileCopyIcon />, name: "Copy", click: help },
-  { icon: <SaveIcon />, name: "Save", click: help },
-  { icon: <PrintIcon />, name: "Print", click: help },
-  { icon: <ShareIcon />, name: "Share", click: help },
-];
+//import LoggedNavbar from "../components/Navbar_logged";
+//import authService from "../services/auth.service";
+//import ciao from "../assets/ciaologo.png";
+import girl1 from"../assets/girl1.jpg";
+import boy1 from"../assets/boy1.jpg"
+import boy2 from"../assets/boy2.jpg"
+import logo from"../assets/ciaologo3.png"
+//import authService from "../services/auth.service";
 
 export default function About() {
   return (
-    <>
-      <div className="about">
-        {authService.getCurrentUser() ? <LoggedNavbar /> : <Navbar />}
-        <div>About</div>
-        <div className="menu">
-          <Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
-            <SpeedDial
-              ariaLabel="SpeedDial basic example"
-              sx={{ position: "absolute", bottom: 16, right: 16 }}
-              icon={<SpeedDialIcon />}
-            >
-              {actions.map((action) => (
-                <SpeedDialAction
-                  key={action.name}
-                  icon={action.icon}
-                  tooltipTitle={action.name}
-                  onClick={action.click}
-                />
-              ))}
-            </SpeedDial>
-          </Box>
+  <>
+   <Navbar />         
+   <div className="spacer layer1 flip"></div>
+   <section className="purple">
+        <div className="ani">
+        <h1 className="title">
+              About US<div className="headers"> </div>
+            </h1>
+            <p className="subtitle">
+              what we do 
+            </p>
         </div>
+        </section>
+        <div className="spacer layer1"></div>
+   <div class="about-section">
+  
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+    <img src={girl1}alt="girl1"/>
+      <div class="container">
+        <h2>Prasiddhi Dahal</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>prasiddhidahal1@gmail.com</p>
+        <p><button class="button">Contact</button></p>
       </div>
-    </>
-  );
-}
+    </div>
+    </div>
+    
+  
+
+  <div class="column">
+    <div class="card">
+    <img src={boy1}alt="boy1" />
+      <div class="container">
+        <h2>Shubham Shakya</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>shakyashubham@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+    <img src={boy2}alt="boy2"/>
+      <div class="container">
+        <h2>Saral Sainju</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>saral@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="column">
+    <div class="card">
+    <img src={logo}alt="logo"/>
+      <div class="container">
+        <h2>Kriti Gautam</h2>
+        <p class="title"></p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>gautamkriti@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</>
+  )
+  }
+
+      
+        
