ee5b8c955a24f117f55af22efaa61cd7179a04b5
Kriti <gautamkriti89@gmail.com>
2022-11-05 23:39:08 +0545

    filter added

--- src/components/Artoutput.js
+++ src/components/Artoutput.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Circles from "./effects/Circles";
+import Pointilism from "./effects/Pointilism";
 
 export default function Artoutput(props) {
   console.log(props.which, props.url);
   switch (props.which) {
     case 1:
       return <Circles src={props.url} />;
+      case 2:
+        return <Pointilism src ={props.url}/>;
     default:
       return "";
   }
