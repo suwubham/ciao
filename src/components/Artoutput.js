95d521799670a4e2f9610943180f1e7108b96df4
Shubham Shakya <shubhamshakya369@gmail.com>
2022-11-07 21:32:01 +0545

    added more grids

--- src/components/Artoutput.js
+++ src/components/Artoutput.js
@@ -9,15 +9,15 @@ export default function Artoutput(props) {
   console.log(props.which, props.url);
   switch (props.which) {
     case 1:
-     return <Circles  src={props.url} />;
+      return <Circles src={props.url} />;
     case 2:
-     return <Filter src ={props.url}/>;
-  case 3:
-       return <Imagedots src ={props.url}/>;
-  case 4:
-       return <Imagenoise src ={props.url}/>;
-  case 5:
-      return <Imageglow src ={props.url}/>;
+      return <Filter src={props.url} />;
+    case 3:
+      return <Imagedots src={props.url} />;
+    case 4:
+      return <Imagenoise src={props.url} />;
+    case 5:
+      return <Imageglow src={props.url} />;
     default:
       return "";
   }
--- src/pages/FromImage.js
+++ src/pages/FromImage.js
@@ -4,7 +4,28 @@ import authService from "../services/auth.service";
 import LoggedNavbar from "../components/Navbar_logged";
 import Navbar from "../components/Navbar";
 import Artoutput from "../components/Artoutput";
-import * as Components from "../styles/signincontainer";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  border-radius: 20px;
+  border: none;
+  background: linear-gradient(
+    135deg,
+    rgba(0, 18, 32, 0.4),
+    rgba(0, 18, 32, 0.2)
+  );
+  backdrop-filter: blur(3px);
+  color: #ffffff;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform 80ms ease-in;
+  &:hover {
+    background-color: #c77dff;
+  }
+`;
 
 export default function FromImage() {
   const [url, setUrl] = useState();
@@ -14,16 +35,20 @@ export default function FromImage() {
     const [file] = e.target.files;
     setUrl(URL.createObjectURL(file));
     setWhich(type);
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: "auto",
-    });
+
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: "auto",
+      });
+    }, 1000);
   };
 
   return (
     <>
       <div className="wrapper-image">
         {authService.getCurrentUser() ? <LoggedNavbar /> : <Navbar />}
+
         <section className="animated-grid">
           <div className="image-card">
             <input
@@ -79,20 +104,110 @@ export default function FromImage() {
               }}
             />
           </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 6);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 7);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 8);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 9);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 10);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 11);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 12);
+              }}
+            />
+          </div>
+
+          <div className="image-card">
+            <input
+              type="file"
+              accept="image/*"
+              className="upload"
+              onChange={(e) => {
+                handleType(e, 13);
+              }}
+            />
+          </div>
         </section>
+
         <div className="output">
           <Artoutput which={which} url={url} />
         </div>
 
-        <Components.Button
+        <Btn
           className="upload"
           onClick={() => {
             setUrl("");
             setWhich("");
+            window.scrollTo({ top: 0, behavior: "smooth" });
           }}
         >
           Clear
-        </Components.Button>
+        </Btn>
       </div>
     </>
   );
