118375e0bbc79211c5649181ae24f27fec9eb11f
Kriti <gautamkriti89@gmail.com>
2022-11-09 23:56:19 +0545

    added posterize and RgbTranslate

--- src/components/Artoutput.js
+++ src/components/Artoutput.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Circles from "./effects/Circles";
-import Filter from "./effects/Filter";
+import Invert from "./effects/Invert";
 import Imagedots from "./effects/Imagedots";
 import Imagenoise from "./effects/Imagenoise";
 import Imageglow from "./effects/Imageglow";
+import RgbTranslate from "./effects/RgbTranslate";
+import Posterize from "./effects/Posterize";
 
 export default function Artoutput(props) {
   console.log(props.which, props.url);
@@ -11,13 +13,17 @@ export default function Artoutput(props) {
     case 1:
       return <Circles src={props.url} />;
     case 2:
-      return <Filter src={props.url} />;
+      return <Invert src={props.url} />;
     case 3:
       return <Imagedots src={props.url} />;
     case 4:
       return <Imagenoise src={props.url} />;
     case 5:
       return <Imageglow src={props.url} />;
+    case 6:
+      return <RgbTranslate src={props.url} />;
+    case 6:
+      return <Posterize src={props.url} />;
     default:
       return "";
   }
---
+++ src/components/effects/RgbTranslate.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { useRef, useEffect } from "react";
+import p5 from "p5";
+
+export default function FromImage(props) {
+  const containerRef = useRef();
+
+  const Sketch = (p5) => {
+    let img;
+    p5.preload = () => {
+      img = p5.loadImage(props.src);
+    };
+
+    p5.setup = () => {
+        img.resize(p5.windowWidth,0)
+        p5.createCanvas(img.width, img.height);
+        p5.textSize(40);
+        p5.textStyle(p5.BOLD);
+        p5.fill(255);
+        p5.stroke(0);
+        p5.strokeWeight(8);
+        p5.noLoop();
+    };
+
+    p5.draw = () => {
+        
+     p5.background(255, 224, 130);
+     p5.image(p5.imageRGBTranslate(img, -50, -50, 0, 0, 50, 50), 0, 0);
+    };
+    p5.imageRGBTranslate = (
+        img,
+        rx = 0,
+        ry = 0,
+        gx = 0,
+        gy = 0,
+        bx = 0,
+        by = 0
+      ) => {
+        let imgIn = img.get();
+        let w = img.width;
+        let h = img.height;
+        let imgOut = p5.createImage(w, h);
+        imgIn.loadPixels();
+        imgOut.loadPixels();
+        for (let y = 0; y < h; y++) {
+          for (let x = 0; x < w; x++) {
+            let rxOut = p5.constrain(x + rx, 0, w - 1);
+            let ryOut = p5.constrain(y + ry, 0, h - 1);
+            let ir = 4 * (rxOut + ryOut * w);
+            let gxOut = p5.constrain(x + gx, 0, w - 1);
+            let gyOut = p5.constrain(y + gy, 0, h - 1);
+            let ig = 4 * (gxOut + gyOut * w) + 1;
+            let bxOut = p5.constrain(x + bx, 0, w - 1);
+            let byOut = p5.constrain(y + by, 0, h - 1);
+            let ib = 4 * (bxOut + byOut * w) + 2;
+            let i = 4 * (x + y * w);
+            imgOut.pixels[ir] = imgIn.pixels[i];
+            imgOut.pixels[ig] = imgIn.pixels[i + 1];
+            imgOut.pixels[ib] = imgIn.pixels[i + 2];
+            imgOut.pixels[i + 3] = 255;
+          }
+        }
+        imgOut.updatePixels();
+        return imgOut;
+      }
+      
+
+
+    p5.keyPressed = () => {
+      if (p5.key === "d") {
+        p5.saveCanvas("myCanvas", "jpg");
+      }
+    };
+  };
+
+  useEffect(() => {
+    let inst = new p5(Sketch, containerRef.current);
+    return () => inst.remove();
+  }, [props]);
+
+  return <div ref={containerRef}></div>;
+}
