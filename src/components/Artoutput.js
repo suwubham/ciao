a17dea22cf030c6ea6e981a7e7e54c72f4934aab
Kriti <gautamkriti89@gmail.com>
2022-11-12 16:57:18 +0545

    added images and fixed the error in collage

--- src/components/Artoutput.js
+++ src/components/Artoutput.js
@@ -19,29 +19,29 @@ export default function Artoutput(props) {
     case 1:
       return <Circles src={props.url} />;
     case 2:
-      return <Invert src={props.url} />;
+      return <Imagenoise src={props.url} />;//Invert
     case 3:
-      return <Imagedots src={props.url} />;
+      return <Posterize src={props.url} />;
     case 4:
-      return <Imagenoise src={props.url} />;
+      return <Collage src={props.url} />;
     case 5:
       return <Imageglow src={props.url} />;
     case 6:
       return <RgbTranslate src={props.url} />;
     case 7:
-      return <Posterize src={props.url} />;
+      return <Imagedots src={props.url} />;
     case 8:
         return <Threshold src={props.url} />;
     case 9:
           return <Imagelines src={props.url} />;
     case 10:
-       return <SwapRB src={props.url} />;
+       return <Collage2 src={props.url} />;
     case 11:
         return <Asciiart src={props.url} />;
     case 12:
-        return<Collage src={props.url}/>;
-    case 3:
-        return<Collage2 src={props.url}/>;
+        return<Invert src={props.url}/>;
+    case 13:
+        return<SwapRB src={props.url}/>;
     default:
       return "";
   }
